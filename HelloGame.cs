fbf63f3cf1e5fdbfc4b22045d7fa8f2dd0ec0159
Jaiden Mispy <jaiden@mispy.me>
2015-02-04 03:41:18 +1100

    Split FNA example into separate repo
    Since we can hopefully get it working just off the
    emscripten branch

---
+++ HelloGame.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+
+namespace HelloWorld {
+    class HelloGame : Game {
+        GraphicsDeviceManager graphics;
+
+        public HelloGame() {
+            graphics = new GraphicsDeviceManager(this);
+
+            Content.RootDirectory = "Content";
+
+            graphics.PreferMultiSampling = true;
+            graphics.IsFullScreen = false;
+
+            graphics.PreferredBackBufferHeight = 480;
+            graphics.PreferredBackBufferWidth = 320;
+        }
+
+        protected override void Initialize() {
+            base.Initialize();
+        }
+
+        protected override void LoadContent() {
+            base.LoadContent();
+        }
+
+        protected override void Update(GameTime gameTime) {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime) {
+            base.Draw(gameTime);
+
+            graphics.GraphicsDevice.Clear(Color.BlanchedAlmond);
+        }
+    }
+}
---
+++ JSILProxies/Proxies.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using JSIL.Meta;
+using JSIL.Proxy;
+
+namespace HelloWorld.JSILProxies {
+    [JSProxy(
+        "MonoGame.Utilities.AssemblyHelper",
+        JSProxyMemberPolicy.ReplaceDeclared
+    )]
+    public abstract class AssemblyHelperProxy {
+        [JSReplacement("document.title")]
+        public static string GetDefaultWindowTitle() {
+            throw new NotImplementedException();
+        }
+    }
+
+    [JSProxy(
+        "Microsoft.Xna.Framework.SDL2_GameWindow",
+        JSProxyMemberPolicy.ReplaceDeclared
+    )]
+    public abstract class SDL2_GameWindowProxy {
+        private void INTERNAL_SetIcon(string title) {
+            // Setting favicons is the html's business
+        }
+    }
+
+    [JSProxy(
+    "System.IO.Path",
+    JSProxyMemberPolicy.ReplaceDeclared
+    )]
+    public abstract class PathProxy {
+        public static Char[] GetInvalidFilenameChars() { // XXX maybe implement this in JSIL core
+            return new Char[] { }; 
+        }
+    }
+
+    // not sure it is possible to overcome the GetDelegateForFunctionPointer issue
+    // using proxies alone. possibly some fancy reflection trick I don't know yet
+    /*[JSProxy(
+    "System.Runtime.InteropServices.Marshal",
+    JSProxyMemberPolicy.ReplaceDeclared
+    )]      
+    public abstract class MarshalProxy {
+        private delegate void GenericDelegate();
+        private static void NotImplementedDelegate() {
+            throw new NotImplementedException();
+        }
+
+        public static Delegate GetDelegateForFunctionPointer(IntPtr ptr, Type t) {
+            return new GenericDelegate(NotImplementedDelegate);
+        }
+    }*/
+}
