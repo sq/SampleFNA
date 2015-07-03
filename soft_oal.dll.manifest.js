30a1f217f257756b80461ffb22cb3d31c1beab2f
Jaiden Mispy <jaiden@mispy.me>
2015-07-04 05:48:23 +1000

    split soft_oal out of SDL2.dll.manifest.js

---
+++ soft_oal.dll.manifest.js
@@ -0,0 +1,6 @@
+if (typeof (contentManifest) !== "object") { JSIL.GlobalNamespace.contentManifest = {}; };
+
+contentManifest["soft_oal.dll"] = [
+   ["NativeLibrary", "../../SampleFNA/soft_oal.js"],
+   ["Library", "../../SampleFNA/externs.js"]
+];;
